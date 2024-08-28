f1e791b8c54ab702dbd288ac953840ce014a09a6
Oskar Ersly <mariuscezar9@gmail.com>
2024-08-28 11:18:05 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ExpressionChangedAfterItHasBeenCheckedError cause this.router.navigate android app crash',
+  webDir: 'www'
+};
+
+export default config;
